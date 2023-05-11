3698b2c198d58108fad044f0b88f6a3aac3f65bf
guillhermek <119710522+guillhermek@users.noreply.github.com>
2023-05-11 15:46:09 -0300

    Add files via upload

---
+++ src/GridLayoutDemo.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+/* ESTUDANTE GUilherme Vinicius da Silva 
+IFMT: https://www.slideshare.net/secret/HrtIHOmPpKpmBc
+*/
+public class GridLayoutDemo
+{
+public static void main(String[] args){
+
+    GridLayoutFrame gridLayoutFrame = new GridLayoutFrame();
+    gridLayoutFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE); 
+    gridLayoutFrame.setSize( 300, 200 );
+    gridLayoutFrame.setVisible(true); 
+    }
+} 
